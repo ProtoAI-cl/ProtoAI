328e961fb235447901ba468b8930a916b96bb3dd
JoseFredes <jose.fredes@mayor.cl>
2024-08-24 13:16:20 -0400

    feat: Add OpenAIClient class and API integration for completion generation

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
 from typing import Union
 
+from openAI.client import OpenAIClient
+
 app = FastAPI()
+
 
 
 @app.get("/")
 def read_root():
+    client = OpenAIClient(api_key="")
+    prompt = "Describe cómo funciona el modo pinza en una mano robótica."
+    response = client.create_completion(prompt)
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/train-data")
+def train_data(data: dict):
+    return {"data": data}
+
--- openAI/client.py
+++ openAI/client.py
@@ -1,6 +1,18 @@
-from openai import OpenAI
+import openai
+import openai.resources
 
 class OpenAIClient:
     def __init__(self, api_key):
         self.api_key = api_key
-        OpenAI.api_key = self.api_key
+        openai.api_key = self.api_key
+
+
+    def create_completion(self, prompt, model="gpt-4o-mini", max_tokens=50):
+        print(model)
+        print(prompt)
+        print(max_tokens)
+    
+        print(chat_completion.choices)
+        message_content = chat_completion.choices[0].message.content
+        print(message_content)
+        return chat_completion
