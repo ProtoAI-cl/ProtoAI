5f4976dd2d28940a5098d06610ae51bcd9996117
JoseFredes <jose.fredes@mayor.cl>
2024-08-25 09:27:36 -0400

    Refactor: main.py to improve image processing and response handling

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from openAI.client import OpenAIClient
-from rules.rules import CLOSE_HAND, OPEN_HAND, PINCH_GRIP
-
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes especificar el dominio del frontend en lugar de "*"
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los headers
+)
 
 IMAGE_PROMPT =f"""
 Como una mano protésica con cinco dedos humanos, identificados como FINGERS = {{ 'index_finger': 'Dedo índice', 'middle_finger': 'Dedo medio', 'ring_finger': 'Dedo anular', 'pinky_finger': 'Dedo meñique', 'lower_thumb': 'Gordo bajo', 'upper_thumb': 'Gordo alto' }}, 
@@ -11,7 +20,7 @@ debes generar instrucciones de movimiento precisas para cada motor que controla 
 El 'lower_thumb' controla la posición del pulgar con respecto a la palma, mientras que el 'upper_thumb' controla la flexión del pulgar.
 Tu tarea es analizar la imagen proporcionada y, 
 basándote en los objetos o acciones visibles, generar un conjunto de comandos que describan cómo la mano protésica debe moverse para interactuar de manera óptima con el entorno. 
-El formato de salida debe ser estrictamente una lista de diccionarios en JSON con la siguiente estructura:
+El formato de salida debe ser estrictamente una lista de diccionarios con la siguiente estructura:
 [
     {{"engine": "upper_thumb", "value": 1}},
     {{"engine": "lower_thumb", "value": 0.8}},
@@ -28,62 +37,36 @@ Ten en cuenta las siguientes directrices clave:
 Recuerda que no debes agregar información adicional fuera del formato especificado. El protocolo debe ser claro, preciso, y directamente aplicable a la imagen analizada.
 """
 
-PREVIOUS_RULE = None
-
 @app.get("/")
-def read_root():
-    client = OpenAIClient(api_key="")
-    global PREVIOUS_RULE
-    prompt = IMAGE_PROMPT
-    response = client.process_image(prompt)
-    print(response)
+def ping():
+    return {"response": "OK"}
 
-    # pretify_response = response
-    # if response == PREVIOUS_RULE:
-    #     alternative_responses = {
-    #         "CLOSE_HAND": "OPEN_HAND",
-    #         "OPEN_HAND": "PINCH_GRIP",
-    #         "PINCH_GRIP": "OPEN_HAND"
-    #     }
-    #     pretify_response = alternative_responses[response]
+class image(BaseModel):
+    data: str
+
+@app.post("/upload-image")
+def upload_image(request: image):
+    if request.data:
+        client = OpenAIClient(api_key="")
+        prompt = IMAGE_PROMPT
+        image_data = request.data
+        response = client.process_image2(prompt,image_data)
+        cleaned_response = response.replace("```json", "").replace("```", "").strip()
+        print(cleaned_response)
+        post_data(cleaned_response)
+        return {"response": cleaned_response}
     
-    # PREVIOUS_RULE = pretify_response
+def post_data(instructions):
+    url = "http://10.67.10.73:8080/data"
+    headers = {"Content-Type": "application/json"}
 
-    # returned_rules = {
-    #     "CLOSE_HAND": CLOSE_HAND,
-    #     "OPEN_HAND": OPEN_HAND,
-    #     "PINCH_GRIP": PINCH_GRIP
-    # }
-    # rule = returned_rules[pretify_response]
-    return {"response": response}
+    payload = {"data": instructions}
 
+    response = requests.post(url, json=payload, headers=headers)
 
-
-# @app.get("/test")
-# def train_data(data: dict):
-#     url = "http://10.67.10.73:8080/data"
-#     headers = {
-#         "Content-Type": "application/json",
-#     }
-#     data = [
-#         {"engine": "upper_thumb", "value": 1},
-#         {"engine": "lower_thumb", "value": 0.8},
-#         {"engine": "index_finger", "value": 0.50},
-#         {"engine": "middle_finger", "value": 0},
-#         {"engine": "ring_finger", "value": 0},
-#         {"engine": "pinky_finger", "value": 0},
-#     ]
-
-#     # Convertir la lista de diccionarios en una cadena JSON
-#     engine_status_list_json = {"engine_status_list_json": data}
-
-#     # Incluirla en los parámetros de la consulta en la URL
-#     # params = {
-#     #     "engine_status_list": engine_status_list_json
-#     # }
-
-#     response = requests.post(url, headers=headers, data=engine_status_list_json)
-#     # response = httpx.post(url, headers=headers, data=engine_status_list_json)
-#     return {"external_response": response.json()}
-
-
+    if response.status_code == 200:
+        print("Data sent successfully")
+        print(response.json())
+    else:
+        print("Error sending data")
+        print(response.json())
