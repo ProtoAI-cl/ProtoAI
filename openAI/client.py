6d136f51bf49247910b9b29a9f7d5d05de9ff72a
JoseFredes <jose.fredes@mayor.cl>
2024-08-28 00:14:11 -0400

    refactor: delete unused function

--- openAI/client.py
+++ openAI/client.py
@@ -17,35 +17,9 @@ class OpenAIClient:
         message_content = chat_completion.choices[0].message.content
         return message_content
     
-    def process_image(self, prompt):
-        image_path = './images/puño.jpeg'
-        base64_image = encode_image(image_path)
-
+    def process_image(self, prompt, model , image_url):
         response = openai.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": prompt},
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}",
-                    },
-                    },
-                ],
-                }
-            ],
-            max_tokens=300,
-        )
-        print(response)
-
-        # return response['choices'][0]['message']['content']
-
-    def process_image2(self, prompt, image_url):
-        response = openai.chat.completions.create(
-            model="gpt-4o-mini",
+            model=model,
             messages=[
                 {
                 "role": "user",
